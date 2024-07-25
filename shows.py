3fe595270815c4c149855f1ea08d03e492195df6
Paulo Ortolan <paulo.ortolan@gmail.com>
2024-07-25 17:41:46 +0100

    New files

--- shows.py
+++ shows.py
@@ -1,10 +1,11 @@
 import csv
+import json
 
 from src.config_manager import ConfigManager
 from src.letterboxd_utils import format_duration
 from src.tmdb_integration import TMDBIntegration
 
-CSV_FILE = "shows_2023.csv"
+CSV_FILE = "shows_2024.csv"
 
 def get_episodes_duration_sum(episodes):
     all_runtimes = list(int(episode['runtime']) for episode in episodes)
@@ -20,6 +21,10 @@ def main():
 
         for show_row in show_reader:
             show_data = tmdb_client.get_show_data(show_row["TMDB_ID"], int(show_row["Season"]))
+
+            with open(f'{show_row["TMDB_ID"]}.json', 'w') as show_file:
+                show_file.writelines(json.dumps(show_data))
+                show_file.close()
 
             episodes_sum = get_episodes_duration_sum(show_data['episodes'])
             print(f"{show_row['TV_Show']} Season {show_row['Season']} on {show_row['Source']} has a total time of {format_duration(episodes_sum)}")
